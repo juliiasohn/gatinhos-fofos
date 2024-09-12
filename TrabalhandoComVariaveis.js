4ff55c9d70f1dcabc88f4ef5700daf03170ce3e3
Julia <161714809+juliiasohn@users.noreply.github.com>
2024-09-12 08:42:03 -0300

    Add files via upload

---
+++ TrabalhandoComVariaveis.js
@@ -0,0 +1,9 @@
+//console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+// JS é case sensitive
+
+idade =29;
+Idade =30;
+const idade = 29
+const nome = "Ricardo"
+let ano = 2020
